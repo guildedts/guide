6e8b88735b4fc5d38fad65663625c6e0a2b989f6
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-09-21 22:02:28 +0100

    Untrack files in .gitignore

---
+++ guide/.vuepress/.temp/pages/creating-your-bot/creating-commands.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-07d8cd44\",\"path\":\"/creating-your-bot/creating-commands.html\",\"title\":\"Creating commands\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":2,\"title\":\"Creating a command\",\"slug\":\"creating-a-command\",\"link\":\"#creating-a-command\",\"children\":[]},{\"level\":2,\"title\":\"Using args\",\"slug\":\"using-args\",\"link\":\"#using-args\",\"children\":[]}],\"git\":{\"updatedTime\":1658687522000},\"filePathRelative\":\"creating-your-bot/creating-commands.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
