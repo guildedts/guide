f801fe78f223124ae48cb8efdfd4df2232531b37
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-09-21 21:59:12 +0100

    idk what im doing bruh

---
+++ guide/.vuepress/.temp/internal/pagesRoutes.js
@@ -0,0 +1,11 @@
+export const pagesRoutes = [
+  ["v-975c0594","/Contributing.html",{"title":"Contributing"},["/Contributing","/Contributing.md"]],
+  ["v-8daa1a0e","/",{"title":"Introduction"},["/index.html","/README.md"]],
+  ["v-07d8cd44","/creating-your-bot/creating-commands.html",{"title":"Creating commands"},["/creating-your-bot/creating-commands","/creating-your-bot/creating-commands.md"]],
+  ["v-6fc8803a","/creating-your-bot/listening-for-events.html",{"title":"Listening for Events"},["/creating-your-bot/listening-for-events","/creating-your-bot/listening-for-events.md"]],
+  ["v-05234d72","/creating-your-bot/",{"title":"Starting your bot"},["/creating-your-bot/index.html","/creating-your-bot/README.md"]],
+  ["v-7bacc522","/getting-started/publishing-your-bot.html",{"title":"Publishing your bot"},["/getting-started/publishing-your-bot","/getting-started/publishing-your-bot.md"]],
+  ["v-ccdc4da0","/getting-started/",{"title":"Preparation"},["/getting-started/index.html","/getting-started/README.md"]],
+  ["v-242a5de3","/getting-started/setting-up-a-bot.html",{"title":"Setting up a bot"},["/getting-started/setting-up-a-bot","/getting-started/setting-up-a-bot.md"]],
+  ["v-3706649a","/404.html",{"title":""},["/404"]],
+]
