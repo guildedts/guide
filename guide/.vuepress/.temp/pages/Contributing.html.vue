42b849a265530715c42a195aeb4731e419fe44ec
Danielius Vaisnoras <dvaisnoras75@gmail.com>
2022-09-21 22:02:28 +0100

    Untrack files in .gitignore

---
+++ guide/.vuepress/.temp/pages/Contributing.html.vue
@@ -0,0 +1,11 @@
+<template><div><h1 id="contributing" tabindex="-1"><a class="header-anchor" href="#contributing" aria-hidden="true">#</a> Contributing</h1>
+<p>Contributing helps us keep this Guide be as tidy and up-to-date as possible. All contributions are greatly appreciated.</p>
+<p>We utilize <a href="https://v2.vuepress.vuejs.org" target="_blank" rel="noopener noreferrer">VuePress<ExternalLinkIcon/></a> to build the Guide. We also write our Guide in <code v-pre>MarkDown</code> files. If you want to contribute we highly recommend knowing the basics of these two.</p>
+<h2 id="getting-started" tabindex="-1"><a class="header-anchor" href="#getting-started" aria-hidden="true">#</a> Getting started</h2>
+<div class="language-bash ext-sh"><pre v-pre class="language-bash"><code><span class="token comment"># Install all dependencies</span>
+<span class="token function">pnpm</span> <span class="token function">install</span>
+<span class="token comment"># Run vuepress in dev mode</span>
+<span class="token function">pnpm</span> dev
+</code></pre><div class="highlight-lines"><br><div class="highlight-line">&nbsp;</div><br><div class="highlight-line">&nbsp;</div></div></div></div></template>
+
+
